123f6f119cd805c319ab3528a67d3d682c12f585
Ana Beltran <124410707+anazbeltran@users.noreply.github.com>
2023-03-28 18:10:23 -0600

    practica 13 24/03/23

--- categorias.sql
+++ categorias.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 28-02-2023 a las 01:45:47
--- Versión del servidor: 10.1.39-MariaDB
--- Versión de PHP: 7.3.5
+-- Tiempo de generación: 25-03-2023 a las 02:47:00
+-- Versión del servidor: 10.4.25-MariaDB
+-- Versión de PHP: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `tienda_online11`
+-- Base de datos: `tiendaenlinea`
 --
 
 -- --------------------------------------------------------
@@ -39,11 +38,11 @@ CREATE TABLE `categorias` (
 --
 
 INSERT INTO `categorias` (`id`, `nombre`, `descripción`) VALUES
-(1, 'SHEIN Frenchy Top de tirantes con estampado floral bajo con fruncido', 'Color:	Verde\r\nEstilo:	Bohemio\r\nTipo de Estampado:	Floral, todo estampado\r\nDetalles:	Nudo, Bajo con volante, Volante fruncido, Fruncido\r\nLongitud:	Corto\r\nTipo:	Tirantes\r\nEscote:	Tirantes finos\r\nCorte d'),
-(2, 'SHEIN Frenchy Pantalones unicolor con bolsillo oblicuo', 'Color:	Verde\r\nEstilo:	Elegante\r\nTipo de Estampado:	Liso\r\nTipo:	Talle alto\r\nTipo de cierre:	cremallera\r\nDetalles:	Cremallera\r\nLínea del Talle de la Cintura:	Talle alto\r\nLongitud:	Largo\r\nTipo de ajuste:'),
-(3, 'SHEIN Frenchy Vestido corpiño con fruncido con estampado floral ribete fruncido', 'Color:	Multicolor\r\nEstilo:	Bohemio\r\nTipo de Estampado:	Floral, todo estampado\r\nDetalles:	Capas relajadas, Volante fruncido, Nudo, Fruncido\r\nTipo:	A línea\r\nEscote:	escote cuadrado\r\nLongitud de la Manga'),
-(4, 'SHEIN VCAY Top de punto de color combinado de espalda abierta con cordón halter', 'Color:	Multicolor\r\nEstilo:	Bohemio\r\nTipo de Estampado:	Color combinado\r\nEscote:	Halter\r\nDetalles:	Espalda al aire, Cordón de espalda\r\nLongitud de la Manga:	sin mangas\r\nLongitud:	Corto\r\nTipo de ajuste:'),
-(11, 'SHEIN VCAY Falda de punto con patrón de cheurón bajo con fleco', 'Color:	Multicolor\r\nEstilo:	Bohemio\r\nTipo de Estampado:	cheurón\r\nDetalles:	Cordón de espalda\r\nLínea del Talle de la Cintura:	Talle alto\r\nLongitud:	Corto\r\nTejido:	Elástico Ligero\r\nMaterial:	Lana Peinada');
+(1, 'Librero Minimalista', 'IDEAL PARA ORGANIZAR, ESPACIOS ABIERTOS, RESISTENTE Y MULTIFUNCIÓN, FÁCIL DE ARMAR, FACIL DE ARMAR, DE MADERA.'),
+(2, 'Silla Escritorio', 'Silla Escritorio para Oficina EJECUTIVA GIRATORIA con COLCHON ACOLCHONADO Y Soporte Lumbar Super Comoda Altura Ajustable'),
+(3, 'Lamparas', 'Juego de 2 lámparas táctiles de mesita de noche pequeña, 3 vías, regulables, puertos USB C, A y tomacorrientes, modernas para habitación de niños, con pantalla de lino y base plateada, color blanco'),
+(4, 'Alexa', 'Nuevo Echo Dot (5.ª generación, modelo de 2022) | Bocina inteligente con Alexa | Negro'),
+(5, 'Espejo con luz', 'Redlemon Espejo con Luz para Makeup Extra Grande Hollywood Vintage (44x35 cm), Espejo para Maquillaje con 9 Focos LED Regulables de Luz Blanca, Cálida y Neutra, Conexión USB, Soporte Ajustable 360°');
 
 --
 -- Índices para tablas volcadas
@@ -63,7 +62,7 @@ ALTER TABLE `categorias`
 -- AUTO_INCREMENT de la tabla `categorias`
 --
 ALTER TABLE `categorias`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
