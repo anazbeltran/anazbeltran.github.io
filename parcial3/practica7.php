f5d497610e334a82e18227238f2373db8da443d0
Ana Beltran <124410707+anazbeltran@users.noreply.github.com>
2023-06-15 19:04:34 -0700

    Add files via upload

--- parcial3/practica7.php
+++ parcial3/practica7.php
@@ -3,18 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Practica 7</title>
-    <link rel="stylesheet" href="css/bootstrap.css">
+    <title>PRACTICA 7</title>
+    <link rel="stylesheet" href="bootstrap.css">
     <style>
         .casilla{
-            height: 50px;
+            height:50px;
         }
         .escalera{
             background-image: url('escalera.png');
             background-size: cover;
         }
-        .serpiente{
-            background-image: url('serpiente.png');
+        .snake{
+            background-image: url('snake.png');
             background-size: 85%;
             background-repeat: no-repeat;
             background-position: center;
@@ -23,27 +23,32 @@
 </head>
 <body>
     <?php
-    $colores = ["#6fa8dc", "#c49cff", "#f9a2d7", "#ff0096", "#f4cccc", "#ffb1d7", "#fff2cc"];
+
+    $colores = [" #8f00b2", "#B23A48", "#8C2F39", "#461220", "#FCB9B2", "#FED0BB", "#AFD0BF"];
     ?>
+
     <div class="container">
-       <h1>Practica 7- Tablero Serpientes Y Escaleras</h1><hr>
-        <form action="">
+        <h1>Practica 7 Tablero de serpientes y escaleras</h1><hr>
+        <form action="practica7.php" method="post">
+            <input type="submit" value="Jugar" class="btn btn-success">
             <div class="row">
-            <?php
-               for($i=100; $i>0; $i--){
-                if($i == 4 || $i == 9 || $i == 15 || $i == 19 || $i == 24 || $i == 29  ){
-                    echo "<div class='col-1 card m-1 casilla escalera' style='background-color:".$colores[rand(0,6)].";'>".$i."</div>";
-                }else if($i == 5 || $i == 10 || $i == 16 || $i == 20 || $i == 25 || $i == 30  ){
-                        echo "<div class='col-1 card m-1 casilla serpiente' style='background-color:".$colores[rand(0,6)].";'>".$i."</div>";
-                   
-                }else{
-                echo "<div class='col-1 card m-1 casilla' style='background-color:".$colores[rand(0,6)].";'>".$i."</div>";
-               }
-            }
-        
-            ?>
+                <?php
+
+                for($i=100; $i>0; $i--){
+                    if($i == 22 || $i == 25 ||$i == 42 ||$i == 12 ||$i == 5){
+                        echo "<div class='col-1  card m-1 casilla escalera' style= 'background-color:".$colores[rand(0,6)].";'>".$i. "</div>";
+                    }else if($i == 23 || $i == 26 || $i == 45 || $i == 10 || $i == 1){
+                        echo "<div class='col-1  card m-1 casilla snake' style= 'background-color:".$colores[rand(0,6)].";'>".$i. "</div>";
+                    }else{
+                        echo "<div class='col-1  card m-1 casilla' style= 'background-color:".$colores[rand(0,6)].";'>".$i. "</div>";
+                    }
+
+                }
+
+                ?>
             </div>
         </form>
     </div>
+
 </body>
 </html>
