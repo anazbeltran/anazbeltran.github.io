0ce31aab032fff3f3cd2171d8c5f964951dd51f6
Ana Beltran <124410707+anazbeltran@users.noreply.github.com>
2023-06-21 19:19:29 -0700

    Add files via upload

--- parcial3/practica8.php
+++ parcial3/practica8.php
@@ -6,19 +6,58 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Practica 8 || La loteria</title>
     <link rel="stylesheet" href="bootstrap.css">
+    <style>
+         .ficha{
+           /* background-image: url("jugador.png");*/
+            background-size: contain;
+            background-repeat: no-repeat;
+            background-position: center;
+        }
+    </style>
 </head>
 <body>
     <div class="container">
         <h1>Juego de la loteria</h1><hr>
         <form action="practica8.php" method="POST">
+            <?php
+                if(isset($_POST["carta1"])){
+                    $carta1= $_POST["carta1"];
+                    $carta2= $_POST["carta2"];
+                    $carta=rand(1,54);
+                    $cartas_dadas=[];
+                    array_push($cartas_dadas,$carta);
+                }else{
+                    $carta1=[];
+                    $total=0;
+                    while($total<16){
+                        $numero=rand(1,64);
+                        if(array_search($numero, $carta1)===false){
+                            $carta1[$total]=$numero;
+                            $total++;
+                        }
+                    }
+                    $carta2=[];
+                    $total=0;
+                    while($total<16){
+                        $numero=rand(1,64);
+                        if(array_search($numero, $carta1)===false){
+                            $carta2[$total]=$numero;
+                            $total++;
+                        }
+                    }
+                }
+            ?>
             <input type="submit" value="Dar carta" name="carta" class="btn btn-primary"><hr>
             <div class="row">
-                <div class="col-6">
+                <div class="col-6 ficha color">
                     <?php
                         //pintar carta 1
                         for($i=0; $i<16; $i++){
-                            $numero= rand(1,54);
-                            echo "<img src='fotos_loteria/".$numero.".jpeg' class='img-thumbnail' width='25%'>";
+                            if(array_search($carta1[$i], $cartas_dadas)=== false){
+                                echo "<img src='fotos_loteria/".$numero.".jpeg' class='img-thumbnail' width='25%'>";
+                            }else{
+                                echo "<img src='fotos_loteria/".$numero.".jpeg' class='img-thumbnail' width='25%'>";
+                            }
                         }
                     ?>
                 </div>
